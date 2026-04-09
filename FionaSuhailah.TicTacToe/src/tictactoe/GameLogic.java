760db26b5e38af8554eac54a4751024dbde1bfce
Lankzin <lankziiin5@gmail.com>
2026-04-09 08:58:36 -0400

    check win

--- FionaSuhailah.TicTacToe/src/tictactoe/GameLogic.java
+++ FionaSuhailah.TicTacToe/src/tictactoe/GameLogic.java
@@ -2,7 +2,55 @@ package tictactoe;
 
 public class GameLogic
 {
-
+	public boolean checkWin(Board board, char player) {
+		for(int row=0; row<3; row++) 
+		{
+			boolean win = true;
+			for(int col=0; col<3; col++) 
+			{
+				if(board.getCell(row,col)!= player) 
+				{
+					win = false;
+				}
+			}
+			if(win) 
+			{
+				return true;
+			}
+			
+		}
+		for(int col=0; col<3; col++) 
+		{
+			boolean win = true;
+			for(int row=0; row<3; row++) 
+			{
+				if(board.getCell(row,col)!= player) 
+				{
+					win = false;
+				}
+			}
+			if(win) 
+			{
+				return true;
+			}
+			
+		}
+		if(board.getCell(0,0) == player && board.getCell(1,1) == player && board.getCell(2,2) == player) 
+		{
+			return true;
+		}
+		if(board.getCell(0,2) == player && board.getCell(1,1) == player && board.getCell(2,0) == player  ) 
+		{
+			return true;
+		}
+		
+		return false;
+		
+	}
 	
 
 }
+
+
+
+
