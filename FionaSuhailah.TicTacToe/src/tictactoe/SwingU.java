b57091607a7684fed71b0712fa6571e26a489c2f
suabdul <sue.ivanna10@gmail.com>
2026-05-19 11:01:20 -0400

    Update SwingU.java

--- FionaSuhailah.TicTacToe/src/tictactoe/SwingU.java
+++ FionaSuhailah.TicTacToe/src/tictactoe/SwingU.java
@@ -66,7 +66,7 @@ public class SwingU extends JFrame {
             setContentPane(buildMenu());
         } else {
             initGame();
-        }
+        } 
 
         addFullscreenToggle();
         setVisible(true);
